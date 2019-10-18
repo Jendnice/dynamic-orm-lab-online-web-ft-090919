01d5a9774b1579dae79fbef2dcd2e2b7ce0856ba
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-18 20:15:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,14 +4,9 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
   
-  # self.column_names.each do |col_name|
-  #   attr_accessor col_name.to_sym
-  # end 
-  
-  def self.attr_accessor
-    self.column_names.each do |col_name|
+  self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
-   end
- end 
+  end 
+  
 
 end
