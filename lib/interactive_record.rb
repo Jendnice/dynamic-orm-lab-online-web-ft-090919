771131420c12939309e55cf7e8c8be79a05e5d1a
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-18 20:06:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -12,9 +12,9 @@ class InteractiveRecord
     
     sql = "pragma table_info('#{table_name}')"
     
-    table_info = DB[:conn].execute(sql)
+    the_table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each do |row|
+    the_table_info.each do |row|
       column_names << row["name"]
     end
     column_names.compact
