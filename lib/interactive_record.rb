0500580beff0f4276d3985648bcd10735f16bba6
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-19 02:14:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,24 +58,28 @@ class InteractiveRecord
     column_name = attribute.keys[0].to_s
     value_name = attribute.values[0]
     
-    sql = "SELECT * FROM #{self.table_name} WHERE XX = ?"
-    DB[:conn].execute(sql, attribute)
+    sql = <<-SQL 
+    SELECT * FROM #{table_name} 
+    WHERE #{column_name} = ?
+    SQL
+    
+    DB[:conn].execute(sql, value_name)
   end 
   
   
   
   
   
-  def self.find_by(attribute)
-    column_name = attribute.keys[0].to_s
-    value_name = attribute.values[0]
+  # def self.find_by(attribute)
+  #   column_name = attribute.keys[0].to_s
+  #   value_name = attribute.values[0]
 
-    sql = <<-SQL
-      SELECT * FROM #{table_name}
-      WHERE #{column_name} = ?
-      SQL
+  #   sql = <<-SQL
+  #     SELECT * FROM #{table_name}
+  #     WHERE #{column_name} = ?
+  #     SQL
 
-    DB[:conn].execute(sql, value_name);
-  end
+  #   DB[:conn].execute(sql, value_name);
+  # end
   
 end
